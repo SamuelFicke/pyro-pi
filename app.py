4139140afb851db6dbf073f093bae499c7adf9ba
samuel ficke <samuel.ficke@gmail.com>
2026-02-16 20:09:32 -0500

    tested with no hardware

--- app.py
+++ app.py
@@ -11,57 +11,92 @@ app = Flask(__name__)
 
 HOUR = 60*60
 MAX_ON_TIME = 4 * HOUR
-DEFAULT_ON_TIME = 1 * HOUR
+DEFAULT_ON_TIME = 15#1 * HOUR
 
 # global variables
-fire_on = False
-on_time = datetime.now()
-off_time = datetime.now()
+class fire_state:
 
-# turn the fireplace off when we start the web app
-fireplace_obj.off()
+    def __init__(self):
+        self.fire_on = False
+        self.time_on = datetime.min
+        self.time_off = datetime.min
+        self.time_off_max = datetime.min
+
+app_state = fire_state()
 
 producer, consumer = multiprocessing.Pipe()
 
+def run_app():
+    # Run the app. Note: disable debug mode in production
+    app.run(debug=False, host='0.0.0.0')
 
-def fireplace_handler(connection):
 
+def fireplace_handler(pipe):
+
+    global state
+
+    # init object
+    fireplace_obj = fireplace(hardware=True)
+    # turn the fireplace off when we start the web app
+    fireplace_obj.off()
+
+    state = fire_state()
+
+    # how often to run the loop
+    loop_time = 5 # seconds
+
+    # run forever
+    while(True):
+
+        # run once every five seconds
+        if(pipe.poll(timeout=loop_time)):
+            form = pipe.recv()
+            
+            if 'set_button' in form:
+                if not state.fire_on:
+                    state.fire_on = True
+                    state.time_off = datetime.now() + timedelta(seconds=DEFAULT_ON_TIME)
+                    state.time_off_max = datetime.now() + timedelta(seconds=MAX_ON_TIME) 
+                    fireplace_obj.on()
+            elif 'unset_button' in form:
+                state.fire_on = False        
+                fireplace_obj.off()
+            elif 'add_hour' in form:
+                state.time_off += timedelta(seconds=HOUR)
+                if state.time_off > state.time_off_max:
+                    state.time_off = state.time_off_max
+            elif 'subtract_hour' in form:
+                state.time_off -= timedelta(seconds=HOUR)
+
+            # send back current state
+            pipe.send(state)
+
+        else:
+            if state.fire_on:
+                # check to see if it's time to turn off the fire
+                current_time = datetime.now()
+                if current_time > state.time_off or current_time > state.time_off_max:
+                    fireplace_obj.off()
+                    state.fire_on = False
+
+                    # send app updated state
+                    pipe.send(state)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+    global app_state
     
-    # grab global variables
-    global fire_on
-    global on_time
-    global off_time
-    global max_off_time
-    global fireplace_obj
-
-    if request.method == 'POST':
-        # Get the value from the form input field named 'new_value'
-        new_value = request.form.get("new_value")
-
-        if 'set_button' in request.form:
-            if not fire_on:
-                fire_on = True
-                off_time = datetime.now() + timedelta(seconds=DEFAULT_ON_TIME)
-                max_off_time = datetime.now() + timedelta(seconds=MAX_ON_TIME) 
-                producer.send("hello")
-                fireplace_obj.on()
-        elif 'unset_button' in request.form:
-            fire_on = False        
-            fireplace_obj.off()
-        elif 'add_hour' in request.form:
-            off_time = off_time + timedelta(seconds=HOUR)
-            if off_time > max_off_time:
-                off_time = max_off_time
-        elif 'subtract_hour' in request.form:
-            off_time = off_time - timedelta(seconds=HOUR)
+    # send form to fireplace handler for processing
+    producer.send(request.form)
+    # wait for current state to come back from fireplace handler
+    if producer.poll():
+        app_state = producer.recv()
 
     # display fire status
-    if fire_on:
+    if app_state.fire_on:
         fire_status_str = "ON"
-        off_time_str = f"Fire will turn off at {off_time.strftime('%I:%M:%S %p')}"
+        off_time_str = f"Fire will turn off at {app_state.time_off.strftime('%I:%M:%S %p')}"
     else:
         fire_status_str = "OFF"        
         off_time_str = ""
@@ -73,21 +108,16 @@ def index():
 def get_time():
     """API endpoint to get the current server time."""
     
-    global fireplace_obj
-    global off_time
-    global fire_on
+    global app_state
 
-    current_time = datetime.now()
+    # read the updated state if it's been updated
+    if producer.poll():
+        app_state = producer.recv()
 
-    # turn fireplace off when we get to that time
-    if fire_on and current_time > off_time:
-        fireplace_obj.off()
-        fire_on = False
-
-    off_time_str = f"Fire will turn off at {off_time.strftime('%I:%M:%S %p')}"
+    off_time_str = f"Fire will turn off at {app_state.time_off.strftime('%I:%M:%S %p')}"
    
     # display fire status
-    if fire_on:
+    if app_state.fire_on:
         fire_status_str = "The Fireplace is ON"
     else:
         fire_status_str = "The Fireplace is OFF"
@@ -95,36 +125,17 @@ def get_time():
 
     return_dict = {"fire_off_time" : off_time_str, "current_time": datetime.now().strftime("%I:%M:%S %p"),"fire_status_str" : fire_status_str}
  
-    print(return_dict)
+    #print(return_dict)
     return return_dict
 
-@app.route('/run-script', methods=['POST'])
-def run_script():
-
-    global fire_on
-    global off_time
-    global on_time
-    global max_off_time
-
-    # Call your Python function when the POST request to /run-script is received
-    result_message = my_script_function()
-    # Get the current date and time
-    current_time_obj = datetime.datetime.now()
-    # Format the time as a string (e.g., "01/20/26, 07:48:00 PM")
-    formatted_time = current_time_obj.strftime("%m/%d/%Y, %I:%M:%S %p")
-    # You can return a message or redirect back to the home page
-    
-    if fire_on:
-        fire_status_str = "ON"
-    else:
-        fire_status_str = "OFF"
-
-    return render_template('index.html', fire_status=fire_status_str)
-    # Or redirect to prevent form re-submission on refresh:
-    # return redirect(url_for('index'))
-
-
 if __name__ == '__main__':
-    # Run the app. Note: disable debug mode in production
-    app.run(debug=False, host='0.0.0.0')
+
+    # we need to run two concurrent threads...one for the web app and one for the fireplace handler
+    # if we only had the web app, we wouldn't be able to turn off the fireplace unless a client was connected to the server
+    fireplace_process = multiprocessing.Process(target=fireplace_handler, args=(consumer,))
+    webapp_process = multiprocessing.Process(target=run_app)
+    fireplace_process.start()
+    webapp_process.start()
+    fireplace_process.join()
+    webapp_process.join()
 
